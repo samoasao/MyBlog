55cd0cb5be55931059a1238242966c6acb8e82fc
Tyler Wright <samoasao@gmail.com>
2017-07-10 06:19:01 +0000

    Fisnish part 1

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,51 +1,48 @@
 class PostsController < ApplicationController
   before_action :find_post, only: [:edit, :update, :show, :delete]
-  
-  #Index action to render all posts
+
+  # Index action to render all posts
   def index
-    @posts = Post.all;
+    @posts = Post.all
   end
-  
-  #New action for creating post
+
+  # New action for creating post
   def new
-    @post = Post.new;
+    @post = Post.new
   end
-  
+
+  # Create action saves the post into database
   def create
     @post = Post.new
-  
     if @post.save(post_params)
-      flash[:notice] = 'Post was successfully created.'
+      flash[:notice] = "Successfully created post!"
       redirect_to post_path(@post)
     else
       flash[:alert] = "Error creating new post!"
       render :new
     end
-    
   end
-  
-  #Edit action retrieves the post and renders the edit page
+
+  # Edit action retrives the post and renders the edit page
   def edit
-    
   end
-  
-  #Update action updates the post with the new information
+
+  # Update action updates the post with the new information
   def update
-    if post.update_attributer(post_params)
+    if @post.update_attributes(post_params)
       flash[:notice] = "Successfully updated post!"
-      redirect_to posts_path(@post)
+      redirect_to post_path(@posts)
     else
       flash[:alert] = "Error updating post!"
       render :edit
     end
   end
-  
-  #Show action renders the individual post after retrieving the id
+
+  # The show action renders the individual post after retrieving the the id
   def show
-    
   end
-  
-  #the destroy action permenently deletes the post
+
+  # The destroy action removes the post permanently from the database
   def destroy
     if @post.destroy
       flash[:notice] = "Successfully deleted post!"
@@ -54,16 +51,13 @@ class PostsController < ApplicationController
       flash[:alert] = "Error updating post!"
     end
   end
-  
-  
-  
+
   private
-  
+
   def post_params
-    #I dont understand this line - tyler
-    params.require(:post).permit(:title, :body) 
+    params.require(:post).permit(:title, :body)
   end
-  
+
   def find_post
     @post = Post.find(params[:id])
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170710045743) do
+ActiveRecord::Schema.define(version: 20170710055744) do
+
+  create_table "ckeditor_assets", force: :cascade do |t|
+    t.string   "data_file_name",               null: false
+    t.string   "data_content_type"
+    t.integer  "data_file_size"
+    t.string   "type",              limit: 30
+    t.integer  "width"
+    t.integer  "height"
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
+    t.index ["type"], name: "index_ckeditor_assets_on_type"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
